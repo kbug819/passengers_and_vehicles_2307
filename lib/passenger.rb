0b1f93783ccae33caa92eafe3cbf4b20d4d6cb06
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-07-19 10:20:30 -0500

    feat: drive and driver? methods

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -4,5 +4,22 @@ class Passenger
   def initialize(passenger_info)
     @name = (passenger_info)["name"]
     @age = (passenger_info)["age"]
+    @is_driver = false
+  end
+
+  def adult?
+    if @age >= 18
+      true
+    else
+      false
+    end
+  end
+
+  def driver?
+    @is_driver
+  end
+
+  def drive
+    @is_driver = true
   end
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Passenger do
     @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     @taylor = Passenger.new({"name" => "Taylor", "age" => 12})   
   end
-  
+
   describe "#initialize" do
     it "instantiates a new object" do
     expect(@charlie).to be_an_instance_of(Passenger)
@@ -18,4 +18,30 @@ RSpec.describe Passenger do
       expect(@charlie.age).to eq(18)
     end
   end
+
+  describe "#adult?" do
+    it "shows if passenger is an adult" do
+      expect(@charlie.adult?).to eq(true)
+      expect(@taylor.adult?).to eq(false)
+    end
+  end
+
+  describe "#driver?" do
+    it "returns if passenger is the driver" do
+      expect(@charlie.driver?).to eq(false)
+      @charlie.drive
+      expect(@charlie.driver?).to eq(true)
+    end
+  end
+
+  describe "#drive" do
+    it "changes passenger to driver" do
+      expect(@charlie.driver?).to eq(false)
+      @charlie.drive
+      expect(@charlie.driver?).to eq(true)
+    end
+  end
+
+
+      
 end
